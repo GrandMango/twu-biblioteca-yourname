68c13c7a158379007a5369dd5c535dcc1a5f8e04
GrandMango <793499453@qq.com>
2020-04-21 15:36:00 +0800

    complete User class

--- src/com/twu/biblioteca/entities/User.java
+++ src/com/twu/biblioteca/entities/User.java
@@ -1,4 +1,36 @@
 package com.twu.biblioteca.entities;
 
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String[] arr) {
+        if(arr.length != 2){
+            System.out.println("ERROR: User Initial Error, invalid input");
+        } else {
+            username = arr[0];
+            password = arr[1];
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
